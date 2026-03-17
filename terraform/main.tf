6295af847a8a875a93f2c49492f0bfe5e3f4ab26
Willem Toemen <willemtoemen@live.nl>
2026-03-17 22:22:03 +0100

    Update Docker container resource with command
    Add command to keep the Docker container running.

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,10 +16,14 @@ provider "docker" {
   host = "ssh://root@192.168.2.28:22"
 }
 
-# Example resource
 resource "docker_container" "dotnet_app" {
   name  = "dotnet_backend"
   image = "mcr.microsoft.com/dotnet/aspnet:8.0"
+  
+  # This keeps the container open by starting a dummy process
+  # Only use this until you have your actual app .dll ready to run
+  command = ["tail", "-f", "/dev/null"] 
+  
   ports {
     internal = 80
     external = 5000
